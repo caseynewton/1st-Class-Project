1e6ecfcd076777650fd22343407c7328b3956719
caseynewton <94085930+caseynewton@users.noreply.github.com>
2021-11-23 13:46:42 -0500

    Video Data Load

---
+++ Video Data Load.sql
@@ -0,0 +1,6 @@
+INSERT INTO videos_cn (title, lengh_in_min, url, views)
+VALUES ('The Tudors - A Complete History of the Tudor Dynasty Documentary',338,'https://www.youtube.com/watch?v=xQDI7g1Tj_4',579926),
+	   ('History of the Black Death - Full Documentary',63,'https://www.youtube.com/watch?v=HYNB4sAxemk',1716948),
+	   ('The Great Tudor Plot To Seize The Crown',45,'https://www.youtube.com/watch?v=KdbvCtmmvAI',780883),
+	   ('VERSAILLES DIRTY SECRETS - Toute LHistoire',52,'https://www.youtube.com/watch?v=BWYIgqMEE2o',7689513),
+	   ('The Mysterious Murder In The Tudor Court',157,'https://www.youtube.com/watch?v=B5INmdOmgs4',126717);
