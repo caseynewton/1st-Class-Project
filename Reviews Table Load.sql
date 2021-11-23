6dbb0afb273a75c0772c8e496cfd006ca6cba0a4
caseynewton <94085930+caseynewton@users.noreply.github.com>
2021-11-23 13:48:15 -0500

    Reviews Table Load

---
+++ Reviews Table Load.sql
@@ -0,0 +1,19 @@
+INSERT INTO reviews_cn (id, reviewer, review, rating)
+VALUES (5,'Nancy Maschio','I love watching your videos.',4),
+(5,'narmale','tens of thousands of dollars he spends on her, yawn',2),
+(5,'Love, Judy','I love this channel',5),
+(4,'Nicole Roth','Overall very interesting documentary. ',3),
+(4,'Anika Joy','This was a really well made video',4),
+(4,'Tyler Durden','I consistently go back to this documentary, it never gets old',3),
+(4,'Doctor Mabuse','Too much information',1),
+(3,'Trev Mason','When its past even medieval standards you know somethings wrong.',1),
+(3,'Carol Lever','The acting is impressive I must say I love  this',5),
+(3,'Patricia Parker','This puts all previous presentations in a more perceptible perspective. Wonderful.',4),
+(1,'Tim Pancale','Too long!',2),
+(1,'Professional Watcher','This is how I know I am too obsessed with this family when I willingly clicked on this 5 hour video.',5),
+(1,'Wicked Tess','Almost 6 hours! Are you kidding me?! Where have you been all my life?! Love it!',5),
+(1,'Beverly Crosby','Absolutely too long',2),
+(1,'Constance Carper','Simply brilliant - a very interesting and educational video!',3),
+(2,'Scorpions Oracle','Boring',1),
+(2,'bianca macsim','Ugh.. enough documentaries on the tudors!!!',3);
+
